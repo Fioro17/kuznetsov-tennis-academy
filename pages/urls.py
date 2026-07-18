8bfc32d9769fce13ff836d7c184f736eea4fa77d
Fedor <fredsmith1703@gmail.com>
2026-07-18 10:51:54 -0400

    Worked on the design of Home and Academy pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('academy/', views.academy, name='academy')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')
+
+def academy(request):
+    return render(request, 'pages/academy.html')
